56e9a2fc18b5b58ff7737134f151c7948e1ad348
l33tarrgin <oleg100petrov@gmail.com>
2020-10-09 02:11:57 +0300

    Integrated a gunslinger's competition in a view-module Refactored few functions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import logic
+from homework_shooting_range import display_winner, competition
 
 app = Flask(__name__)
 
@@ -9,22 +10,26 @@ response2 = "<script> alert( 'Привет, мир!' );</script>"
 response3 = "<div> <p> \"О дивный новый\" </p></div>"
 
 
-
 @app.route('/')
-def hello_world():
+def show_home():
     return render_template('home.html', context=app.config)
 
 
-@app.route('/some_page')
-def foo():
-    return render_template('home.html', context=response1)
+@app.route('/game')
+def show_game():
+    data = competition()
+    data = display_winner(data)
+    for i, table in enumerate(data):
+        table_ = table.to_html()
+        data[i] = table_.replace('\n', '')
+    print(*map(type, data))
+    return render_template('home.html', context=data)
 
 
-@app.route('/links')
-def foo2():
+@app.route('/about')
+def show_about():
     return render_template('home.html', context=response2)
 
 
-
 if __name__ == '__main__':
-    app.run(debug=1)
+    app.run()
