01c82de8cf20f1b17c5e9efc5d61cb752089885c
l33tarrgin <oleg100petrov@gmail.com>
2020-11-10 23:16:57 +0300

    installed and set flask-migrate

--- application/__init__.py
+++ application/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import application.config as config
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_pyfile('config.py')
+    app.config.from_object(config.DevelopmentConfig)
 
     db.init_app(app)
+    migrate.init_app(app, db,)
 
     with app.app_context():
         from . import routes  # Import routes
