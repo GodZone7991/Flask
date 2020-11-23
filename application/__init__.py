3b3ba9c118934029a6830a789d3a80dcfa68e3a6
l33tarrgin <oleg100petrov@gmail.com>
2020-11-24 01:22:42 +0300

    Registered blueprints in a core file of app

--- application/__init__.py
+++ application/__init__.py
@@ -17,7 +17,22 @@ def create_app():
     migrate.init_app(app, db, directory='migrations')
 
     with app.app_context():
-        from . import routes  # Import routes
+        from application.core.core import core_bp
+        from .bot.bot import bot_bp
+        from .playlist.playlist import playlist_bp
         db.create_all()  # Create sql tables for our data models
-
+        app.register_blueprint(core_bp)
+        app.register_blueprint(bot_bp)
+        app.register_blueprint(playlist_bp)
         return app
+
+# TODO: Интерфейс парсинга плейлиста
+# TODO: Интерфейс сохранения данных парсинга в БД
+# TODO: Интерфейс вызова данных из БД и составления запроса к API
+# TODO: Интрефейс обработки ответа API и сохранения в БД
+# TODO: Интерфейс передачи из БД в сисетему ML
+# TODO: Интерфейс приема данных от ML и записи в БД (mood set)
+# TODO: Интерфейс генерации плейлиста для пользователя на основе mood set'а из БД
+# TODO: продумать модель с использованием прокси-параметров для поиска в базе spotify
+# TODO: опробовать модель с избыточным числом параметров
+# TODO: Посмотреть документацию к TensorFlow, прикрутить тест оценки настроения,
