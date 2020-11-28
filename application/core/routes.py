717be61123480f97e86d7d8c1032a301428d8015
l33tarrgin <oleg100petrov@gmail.com>
2020-11-28 22:12:27 +0300

    Changed type annotation in routes.py, changed content in index.html template

--- application/core/routes.py
+++ application/core/routes.py
@@ -3,14 +3,14 @@ from .core import core_bp
 
 
 @core_bp.route('/')
-def home():
+def home() -> render_template:
     """Shows indexing homepage."""
     title = 'PsychoMusic'
     return render_template('index.html', title=title)
 
 
 @core_bp.route('/about')
-def about():
+def about() -> render_template:
     """Shows indexing page with info about us."""
     title = 'About Us'
     return render_template('index.html', title=title)
