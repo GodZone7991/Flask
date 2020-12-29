59329cbea83c97da813f1893ab53cb6c64b2c1f9
l33tarrgin <oleg100petrov@gmail.com>
2020-12-29 23:59:52 +0300

    Added functions for handling of json-like caches

--- application/utils.py
+++ application/utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 def create_caches(caches_path: str) -> None:
@@ -9,3 +10,25 @@ def create_caches(caches_path: str) -> None:
     """
     if not os.path.exists(caches_path):
         os.makedirs(caches_path)
+
+
+def write_cache(cache_file, data: dict) -> None:
+    """
+    The function writes data into a json-like file.
+    :param cache_file: a file object
+    :param data: a json-like object, e.g. dict
+    :return: None
+    """
+    with open(cache_file, 'w', encoding='utf-8') as file:
+        json.dump(data, file)
+
+
+def read_cache(cache_file) -> dict:
+    """
+    The function reads data from a cache file.
+    :param cache_file: a file object
+    :return: data: a json-like object e.g. dict
+    """
+    with open(cache_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
